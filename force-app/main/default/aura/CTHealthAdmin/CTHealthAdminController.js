673bee3ffbe79a2e159618235b0b4f0e36e00df0
jamy155 <jarhudaugustin@gmail.com>
2025-03-17 08:19:50 -0400

    ADDED tabSelectHander method in CTHealthAdmin & ADDED createRecord method in CTHealthHeader

--- force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
+++ force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
@@ -1,5 +1,11 @@
 ({
-    myAction : function(component, event, helper) {
-
+  tabSelectHander: function (component, event, helper) {
+    const selectedTabId = event.getParam("id");
+    if (selectedTabId == "person") {
+      component.set("v.headerTitle", "Person View");
+    } else {
+      component.set("v.headerTitle", "Location View");
     }
-})
+    component.set("v.scope", selectedTabId);
+  }
+});
--- force-app/main/default/aura/CTHealthHeader/CTHealthHeaderController.js
+++ force-app/main/default/aura/CTHealthHeader/CTHealthHeaderController.js
@@ -1,5 +1,11 @@
 ({
-    myAction : function(component, event, helper) {
+  createRecord: function (component, event, helper) {
+    const scope = component.get("v.scope");
+    var createRecordEvent = $A.get("e.force:createRecord");
 
-    }
-})
+    createRecordEvent.setParams({
+      entityApiName: scope === "person" ? "Person__c" : "Location__c"
+    });
+    createRecordEvent.fire();
+  }
+});
