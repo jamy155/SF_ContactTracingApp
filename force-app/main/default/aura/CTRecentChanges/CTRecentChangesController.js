d48027acdddb6bc41f079c3133730292851ab0c2
jamy155 <jarhudaugustin@gmail.com>
2025-03-20 01:45:10 -0400

    ADDED searchRecord method in CTRecentChangesController

--- force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
+++ force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
@@ -37,5 +37,18 @@
     }
 
     helper.fetchRecentHealthChanges(component);
+  },
+
+  handleKeyUp: function (component, event, helper) {
+    const isEnterKey = event.keyCode === 13;
+    var queryTerm = component.find("enter-search").get("v.value");
+
+    if (!queryTerm) {
+      component.set("v.data", component.get("v.initialRespone"));
+    }
+    if (isEnterKey) {
+      component.set("v.issearching", true);
+      helper.searchRecord(component, queryTerm);
+    }
   }
 });
